09bfdb9ac8b7c76619bdf9bb3712f499a1bdf39d
phillmac <phillmackintosh@gmail.com>
2021-05-22 08:04:53 +0000

    Add tracker raw stats

---
+++ tracker-events.php
@@ -0,0 +1,18 @@
+<?php
+    require('functions.php');
+
+    $address = upstreamAddress('ERRORS');
+    $handle = FALSE;
+
+    $handle = fopen($address, 'r');
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    if ( !$handle ) {
+        http_response_code(500);
+        json_encode(array('status' => 'error'));
+    }  else {
+        while(!feof($handle)) {
+            echo fgets($handle, 1024);
+        }
+    }
