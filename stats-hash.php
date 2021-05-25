b5f7c2213cce19decbc3748a1c68aed391310502
phillmac <phillmackintosh@gmail.com>
2021-05-25 06:04:58 +0000

    Remove cli handler

--- stats-hash.php
+++ stats-hash.php
@@ -4,22 +4,31 @@
     $address = upstreamAddress('HASH');
     $handle = FALSE;
 
-    if (isset($_REQUEST['hash'])) {
-        $handle = jsonGet($address, json_encode(array('hash' => $_REQUEST['hash'])));
+    if (! isset($_REQUEST['hash'])) {
+        http_response_code(400);
+        json_encode(
+            array(
+                'status' => 'error',
+                'reason' => "Missing 'hash' param"
+            )
+        );        
+        die();
     }
-    else {
-        $params = json_decode(file_get_contents('php://input'), true);
-        if (isset($params['hash'])) {
-            $handle = jsonGet($address, json_encode(array('hash' => $params['hash'])));
-        }
-    }
+    
+    $handle = jsonGet($address, json_encode(array('hash' => $_REQUEST['hash'])));
+
     
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json; charset=utf-8');
 
-    if ( !$handle ) {
+    if (! $handle) {
         http_response_code(500);
-        json_encode(array('status' => 'error'));
+        json_encode(
+            array(
+                'status' => 'error',
+                'reason' => 'Invalid upstream handle'
+            )
+        );
     }  else {
         while(!feof($handle)) {
             echo fgets($handle, 1024);
