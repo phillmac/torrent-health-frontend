929d54c27f32efa81cfed6e0329dea294ad7a825
phillmac <phillmackintosh@gmail.com>
2021-05-26 07:19:09 +0000

    Debug

--- tracker-stats.php
+++ tracker-stats.php
@@ -58,7 +58,8 @@
                 '$test' => $test,
                 'count' => count($test),
                 'is_array' => is_array($test) ? 'true' : 'false',
-                'is_object' => is_object($test) ? 'true' : 'false'
+                'is_object' => is_object($test) ? 'true' : 'false',
+                'var_dump' => var_dump($test)
             )    
         );
        ?>
